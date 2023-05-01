512f6eb05be147905e58fe33f8e7b73da2713bb6
NathanCardoso <nathan.cardoso@codezone.com.br>
2023-05-01 13:52:52 -0300

    correction in service-worker

--- service-worker.js
+++ service-worker.js
@@ -1,39 +1,62 @@
 "use strict";
 
-// Um Service Worker se comporta como um servidor proxy situado entre uma aplicação web, o navegador e a rede 
-// (quando esta estiver disponível). Eles servem, dentre outras coisas, para possibilitar a criação de experiências 
-// offline eficientes, interceptar requisições de rede – agindo adequadamente de acordo com o status atual da conexão – 
-// e atualizar os assets que residem no servidor. 
-
-
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
 
 const FILES_TO_CACHE = [
   "/",
   "/index.html",
-  "/Aluno.js",
-  "/AlunoDTO.js",
-  "/CtrlManterAlunos.js",
-  "/CtrlSessao.js",
-  "/DaoAluno.js",
-  "/ModelError.js",
-  "/ViewerAluno.js",
-  "/ViewerError.js",
-  "/favicon.ico",
-  "/manifest.json",
-  "/style.css"
+	"/request-to-ride.html",
+	"/ride.html",
+	"/ride-confirmed.html",
+	"/src/css/components/header.css.css",
+	"/src/css/components/aside.css",
+	"/src/css/components/footer.css",
+	"/src/css/home/header",
+	"/src/css/home/home.css",
+	"/src/css/home/main.css",
+	"/src/css/home/style-home.css",
+	"/src/css/request/main.css",
+	"/src/css/request/request.css",
+	"/src/css/request/style-request.css",
+	"/src/css/ride/main.css",
+	"/src/css/ride/ride.css",
+	"/src/css/ride/style-ride.css",
+	"/src/css/ride-confirmed/card-details.css",
+	"/src/css/ride-confirmed/modal.css",
+	"/src/css/ride-confirmed/style-ride-confirmed.css",
+	"/src/css/utils/color.css",
+	"/src/css/utils/container.css",
+	"/src/css/reset.css",
+	"/src/css/style.css",
+	"/src/fonts/stylesheet.css",
+	"/src/fonts/subset-Poppins-Light.eot",
+	"/src/fonts/subset-Poppins-Light.svg",
+	"/src/fonts/subset-Poppins-Light.ttf",
+	"/src/fonts/subset-Poppins-Light.woff",
+	"/src/fonts/subset-Poppins-Light.woff2",
+	"/src/fonts/subset-Poppins-Light-SemiBold.eot",
+	"/src/fonts/subset-Poppins-Light-SemiBold.svg",
+	"/src/fonts/subset-Poppins-Light-SemiBold.ttf",
+	"/src/fonts/subset-Poppins-Light-SemiBold.woff",
+	"/src/fonts/subset-Poppins-Light-SemiBold.woff2",
+	"/src/fonts/stylesheet.css",
+	"/src/icons/192-192.jpeg",
+	"/src/icons/512-512.png",
+	"/src/js/components/btnMobile.js",
+	"/src/js/components/modal.js",
+	"/src/js/script.js",
+	"/src/svg/drive-profile.svg",
+	"/src/svg/home.svg",
+	"/src/svg/list-ride.svg",
+	"/src/svg/star.svg",
+	"/src/svg/student-profile.svg",
 ];
 
 //--------------------------------------------------------------------------------//
 
-// ---> self se refere à janela do navegador
-// Estamos associando um Listener ao evento 'install'
 self.addEventListener("install", evt =>  {
   console.log("[App]Instalação");
-  // * caches é uma variável global que retorna o CacheStorage do contexto atual
-  // * caches.keys retorna uma Promise que retornará um array com o nome de 
-  //   todos objetos armazenados no cache
   caches.keys().then(keyList => {
     return Promise.all(
       keyList.map(key => {
